f1d054119c5839aab16279681ed2df481a70adb6
skyluf <trigunpatil07@gmail.com>
2026-03-22 13:53:18 +0530

    added products via products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,5 @@
 console.log('hello');
 
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090 //price in cents not dollars becuz of float number calculation issues
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
-
 let productsHTML = ``;
 
 products.forEach((product)=> {
@@ -41,8 +15,8 @@ products.forEach((product)=> {
       </div>
 
       <div class="product-rating-container">
-        <img class="product-rating-${product.rating.stars * 10}"
-          src="images/ratings/rating-40.png">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
@@ -78,7 +52,6 @@ products.forEach((product)=> {
         Add to Cart
       </button>
     </div>`;
-
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
