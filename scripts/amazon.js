e523931b03ca25d90793c01a99ecd4a6a1109268
skyluf <trigunpatil07@gmail.com>
2026-03-26 00:26:19 +0530

    added cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -44,11 +44,11 @@ products.forEach((product)=> {
       <div class="product-spacer"></div>
 
       <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
+        <img src="images/icons/checkmark.png">  
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
         Add to Cart
       </button>
     </div>`;
@@ -56,4 +56,30 @@ products.forEach((product)=> {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+
+  button.addEventListener("click",() => {
+    console.log('Added product'); 
+    const productName = button.dataset.productName;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productName === item.productName) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productName: productName,
+          quantity: 1
+        });
+      }
+      console.log(cart);
+
+  });
+});
 
